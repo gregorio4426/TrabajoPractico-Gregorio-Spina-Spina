26d40b9eb844ad78c895839201eb932dd11883f0
pichinitoty <tomasspina11@gmail.com>
2026-06-06 17:30:33 -0300

    endPoint Registrar alumno

--- simulacro_nombreAlumno/src/main/java/utn/simulacro_nombreAlumno/controller/AlumnoController.java
+++ simulacro_nombreAlumno/src/main/java/utn/simulacro_nombreAlumno/controller/AlumnoController.java
@@ -1,9 +1,12 @@
 package utn.simulacro_nombreAlumno.controller;
 import ch.qos.logback.core.read.ListAppender;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utn.simulacro_nombreAlumno.model.Alumno;
+import utn.simulacro_nombreAlumno.model.request.AlumnoRequest;
 import utn.simulacro_nombreAlumno.model.response.AlumnoResponse;
 import utn.simulacro_nombreAlumno.service.AlumnoService;
 
@@ -17,10 +20,19 @@ public class AlumnoController {
     private final AlumnoService alumnoService;
 
 
+    @PostMapping
+    public ResponseEntity<AlumnoResponse> registrar(@Valid @RequestBody AlumnoRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(alumnoService.createAlumno(request));
+    }
+
     @GetMapping
     public ResponseEntity<List<AlumnoResponse>> listarTodos (){
         return ResponseEntity.ok(alumnoService.listarAlumnos());
     }
+
+
+
+
 
 
 }
